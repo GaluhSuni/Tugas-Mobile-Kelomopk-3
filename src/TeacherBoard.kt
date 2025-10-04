32e3096eafc23ed7b4990a610f3b0d77e83c9edc
stepanussaruran <potatocarotz@gmail.com>
2025-10-04 21:33:37 +0700

    Memodifikasi class TeacherBoard

--- src/TeacherBoard.kt
+++ src/TeacherBoard.kt
@@ -1,5 +1,8 @@
 class TeacherBoard(
-
+    val id: Int,
+    val teacherList: MutableList<Teacher> = mutableListOf(),
+    val leader: Teacher? = null
 ) {
-
+    fun addTeacher(teacher: Teacher) = teacherList.add(teacher)
+    override fun toString() = "TeacherBoard(teachers=${teacherList.size}, leader=${leader?.name})"
 }

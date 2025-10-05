eb23136841179b4e9518486d0ee907cba51c9d36
BobyAngga <bobyanggasastrawan222@gmail.com>
2025-10-05 22:10:29 +0700

    class Student

--- src/Student.kt
+++ src/Student.kt
@@ -1,5 +1,8 @@
 class Student(
-
-){
-
+    id: Int,
+    name: String,
+    email: String? = null,
+    val studentNumber: String
+) : Person(id, name, email) {
+    override fun toString() = "Student($studentNumber, $name)"
 }

28d4353277042872b4b78ae25d596c3ed4ddf20f
anastasiafff <160554698+anastasiafff@users.noreply.github.com>
2025-10-06 00:00:33 +0700

    update chair.kt

--- src/Chair.kt
+++ src/Chair.kt
@@ -1,15 +1,15 @@
-class Chair(
-   val id: Int,                   // ID unik untuk setiap kursi
-    val material: String,         
+cclass Chair(
+    val id: Int,                   // ID unik untuk setiap kursi
+    val material: String,          // Bahan kursi
     var isOccupied: Boolean = false // Status kursi (default: tidak ditempati)
 ) {
-  // Fungsi untuk menandai kursi sedang digunakan
+    // Fungsi untuk menandai kursi sedang digunakan
     fun occupy() {
         if (!isOccupied) {
             isOccupied = true
-            println("Chair $id is now occupied.")
+            println("Kursi $id sekarang sedang digunakan.")
         } else {
-            println("Chair $id is already occupied.")
+            println("Kursi $id sudah digunakan.")
         }
     }
 
@@ -17,17 +17,19 @@ class Chair(
     fun release() {
         if (isOccupied) {
             isOccupied = false
-            println("Chair $id is now available.")
+            println("Kursi $id sekarang tersedia.")
         } else {
-            println("Chair $id was already free.")
+            println("Kursi $id sudah kosong.")
         }
     }
 
     // Menampilkan informasi kursi dalam bentuk string
     override fun toString(): String {
-        return "Chair(id=$id, material='$material', isOccupied=$isOccupied)"
+        return "Kursi(id=$id, bahan='$material', sedangDipakai=$isOccupied)"
     }
 }
+
+
 
 
 

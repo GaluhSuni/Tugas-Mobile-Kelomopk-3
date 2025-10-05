5bc11bbbc2ddaed186aa54bda153e30735f2a252
cahyokml <cahyokumolo2415@gmail.com>
2025-10-06 00:20:54 +0700

    menambahkan ulang file yang hilang setelah reset

--- src/Curriculum.kt
+++ src/Curriculum.kt
@@ -1,5 +1,12 @@
 class Curriculum(
-
+    val subjectList: MutableList<Subject>
 ) {
+    fun addSubject(subject: Subject) {
+        subjectList.add(subject)
+    }
 
+    fun showSubjects() {
+        println("Daftar Subject dalam Kurikulum:")
+        subjectList.forEach { println(it.name) }
+    }
 }

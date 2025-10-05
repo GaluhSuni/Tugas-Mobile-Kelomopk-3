9f478fd4392627d4ddf90e53a589cf15847d1edf
JojorMaria <jojorsitompul655@gmail.com>
2025-10-05 15:46:53 +0700

    Update class Course dengan atribut baru

--- src/Course.kt
+++ src/Course.kt
@@ -1,5 +1,7 @@
 class Course(
-
+    val studentList: MutableList<Student> = mutableListOf(),
+    val teacherList: MutableList<Teacher> = mutableListOf(),
+    var subject: Subject? = null
 ) {
 
 }

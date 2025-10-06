e2330efa84d742721188b8b3ab5a325dd1e9b698
NOXDG <galuhsuni0@gmail.com>
2025-10-06 08:42:51 +0700

    Add room class

--- src/Room.kt
+++ src/Room.kt
@@ -1,5 +1,13 @@
 class Room(
-
+    val id: String,
+    val name: String,
+    val tableList: MutableList<Table> = mutableListOf(),
+    val chairList: MutableList<Chair> = mutableListOf(),
+    var CSStaff: Staff? = null // staff in charge of the room
 ) {
+    fun addTable(t: Table) = tableList.add(t)
+    fun addChair(c: Chair) = chairList.add(c)
 
+    //tambahkan set untuk mengubah CSStaff
+    override fun toString() = "Room($name, tables=${tableList.size}, chairs=${chairList.size})"
 }
